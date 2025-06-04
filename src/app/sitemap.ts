ae28dfa8011c6c6ba94840c81b5680d65970a7c4
vitalis maina <mainavitalis65@gmail.com>
2025-06-04 13:13:36 +0300

    updated sitemaps

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseURL = "https://www.spencecreations.co.ke/";
+  const baseURL = "https://www.spencecreations.co.ke";
 
   try {
     return [
